eca9769f13eadf0936f2f27a88e656a191a91d01
Kou Matsumoto <developer@kou-matsumoto.jp>
2022-01-22 18:57:53 +0900

    refactor(services): use es private method rather ts

--- src/services/AppUseCases.ts
+++ src/services/AppUseCases.ts
@@ -6,13 +6,12 @@ import { AppStorage } from './AppStorage';
 import { UseCases } from './interfaces';
 
 export class AppUseCases implements UseCases {
+  readonly #storage: AppStorage;
   readonly #notes = new BehaviorSubject<Note[]>([]);
   readonly #activeEditor = new BehaviorSubject<Editor | null>(null);
 
-  constructor(private readonly storage: AppStorage) {}
-
-  get activeEditor() {
-    return this.#activeEditor.pipe();
+  constructor(storage: AppStorage) {
+    this.#storage = storage;
   }
 
   get hasActiveEditor() {
@@ -20,7 +19,7 @@ export class AppUseCases implements UseCases {
   }
 
   queryNotesWithReloading(): Observable<Note[]> {
-    this.loadNotes();
+    this.#loadNotes();
 
     return this.#notes.pipe(
       map(Note.orderByCreatedNewer),
@@ -31,7 +30,7 @@ export class AppUseCases implements UseCases {
 
   async changeEditorInactiveAndSaveNote(source: Note, updates: Pick<Note, 'text' | 'editorNodes'>) {
     this.#activeEditor.next(null);
-    await this.saveNote(source, updates);
+    await this.#saveNote(source, updates);
   }
 
   async changeEditorActive(editor: Editor) {
@@ -50,16 +49,16 @@ export class AppUseCases implements UseCases {
     this.#activeEditor.getValue()?.customCommands.addSlash();
   }
 
-  private loadNotes() {
-    const notes = this.storage.loadNotes();
+  #loadNotes() {
+    const notes = this.#storage.loadNotes();
     this.#notes.next(notes);
   }
 
-  private async saveNote(source: Note, updates: Pick<Note, 'text' | 'editorNodes'>) {
+  async #saveNote(source: Note, updates: Pick<Note, 'text' | 'editorNodes'>) {
     const newNote = await Note.create({ ...source, text: updates.text, editorNodes: updates.editorNodes });
     const errors = Note.validateUpdates(source, newNote);
     if (errors === null) {
-      const notes = this.storage.saveNote(newNote);
+      const notes = this.#storage.saveNote(newNote);
       this.#notes.next(notes);
     } else {
       console.log('[AppUseCases/updateNote/errors]', { errors, source, updates });
